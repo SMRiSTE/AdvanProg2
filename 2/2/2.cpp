e96ba7b90087a3639fc30e463b13bb1273b78c02
Ш.М.Р. <s.m.r.00800ist.@gmail.com>
2024-06-09 17:22:54 +0300

    Initial commit

--- 2/2/2.cpp
+++ 2/2/2.cpp
@@ -1,13 +1,17 @@
 ﻿#include<iostream>
 
 class smart_array {
+	double re_, im_;
 private:
 	int once = 0;
 	int size = 0;
 	int* arr = new int[size];
 public:
-	smart_array(const smart_array&) = delete;
-	smart_array& operator=(const smart_array&) = delete;
+	smart_array& operator=(const smart_array& other) {
+		re_ = other.re_;
+		im_ = other.im_;
+		return *this;
+	}
 
 	smart_array(int size) {
 		this->size = size;
@@ -31,7 +35,7 @@ public:
 		else {
 			return arr[elem];
 		}
-		
+
 	}
 
 	~smart_array() {
@@ -46,9 +50,12 @@ int main() {
 		arr.add_element(1);
 		arr.add_element(4);
 		arr.add_element(155);
-		arr.add_element(14);
-		arr.add_element(15);
-		std::cout << arr.get_element(1) << std::endl;
+
+		smart_array new_array(2);
+		new_array.add_element(44);
+		new_array.add_element(34);
+
+		arr = new_array;
 	}
 	catch (const std::exception& ex) {
 		std::cout << ex.what() << std::endl;
